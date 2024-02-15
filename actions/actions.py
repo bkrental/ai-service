157ecc6787728eee3aa91c6a0f6ffa5f7038a36d
datdev2409 <datdev2409@gmail.com>
2024-02-15 21:49:35 +0700

    feat: add search_properties form

--- actions/actions.py
+++ actions/actions.py
@@ -4,28 +4,11 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+import requests
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from typing import Any, Text, Dict, List
-
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
 
 
 class ActionSearchProperties(Action):
@@ -34,8 +17,23 @@ class ActionSearchProperties(Action):
 
     def run(self, dispatcher, tracker, domain):
 
+        property_type = tracker.get_slot("property_type")
+        price_lower_bound = tracker.get_slot("price_lower_bound")
+        price_upper_bound = tracker.get_slot("price_upper_bound")
+
+        res = requests.get("https://prod-backend.datnguyen2409.me/posts")
+
+        if res.status_code != 200:
+            return dispatcher.utter_message(response="utter_search_properties_error")
+
+        properties_title = map(lambda p: p["name"], res.json()["data"])
+
+        # dispatcher.utter_message(
+        #     response="utter_search_properties",
+        #     properties="\n".join(properties_title),
+        # )
+
         dispatcher.utter_message(
-            response="utter_search_properties",
-            properties="3 BHK in Whitefield, 2 BHK in Sarjapur, 3 BHK in Electronic City",
+            text=f"Đây là kết quả các {property_type} có giá từ {price_lower_bound} đến {price_upper_bound}"
         )
         return []
